5c6c64eb6d717f666012e7d6e26fb6abc4f72549
Rain44556 <bristy44556@gmail.com>
2023-11-06 12:16:46 +0600

    starsection.js file is added

---
+++ src/components/StarSection/StarSection.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const StarSection = () => {
+    return (
+        <div>
+            <main>
+                <section>
+                    <h1>Relax in a comfortable spot and start your day with your favourite cup of coffee.</h1>
+                    <button>Order</button>
+                </section>
+            </main>
+        </div>
+    );
+};
+
+export default StarSection;
