6d7b55882948b1c1df65807dd1a9605a34f0ff35
Rain44556 <bristy44556@gmail.com>
2023-11-06 16:17:33 +0600

    uniquecatagoy.js file added for uniques coffees or meals

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from "./components/Header/Header.js";
 import StarSection from './components/StarSection/StarSection.js';
+import UniqueCatagory from './components/UniqueCatagory/UniqueCatagory.js';
 
 function App() {
   return (
 <div>
   <Header></Header>
   <StarSection></StarSection>
+  <UniqueCatagory></UniqueCatagory>
 </div>
   );
 }
