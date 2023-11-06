72948e168c7b25e671839a21eb92533522c450c8
Rain44556 <bristy44556@gmail.com>
2023-11-06 16:00:20 +0600

    bg image used and title and button design added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Header from "./components/Header/Header.js";
-
+import StarSection from './components/StarSection/StarSection.js';
 
 function App() {
   return (
 <div>
   <Header></Header>
+  <StarSection></StarSection>
 </div>
   );
 }
--- src/components/StarSection/StarSection.js
+++ src/components/StarSection/StarSection.js
@@ -1,11 +1,12 @@
 import React from 'react';
-
+import './StarSection.css';
 const StarSection = () => {
     return (
         <div>
-            <main>
-                <section>
-                    <h1>Relax in a comfortable spot and start your day with your favourite cup of coffee.</h1>
+            <main className='main' id='main'>
+                <section className='star' id='home' style={{backgroundImage: `url(./images/coffee-bg.jpg)`}}>
+                    <h1>Relax in a comfortable spot and start your day<br/> with
+             your favourite cup of coffee.</h1>
                     <button>Order</button>
                 </section>
             </main>
