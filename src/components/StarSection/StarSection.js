5c9a186cd8d21302b9457386a81673d87bf085f7
Rain44556 <bristy44556@gmail.com>
2023-11-07 21:45:57 +0600

    unique coffees img and title added and css file getting ready

--- src/components/StarSection/StarSection.js
+++ src/components/StarSection/StarSection.js
@@ -4,7 +4,7 @@ const StarSection = () => {
     return (
         <div>
             <main className='main' id='main'>
-                <section className='star' id='home' style={{backgroundImage: `url(./images/coffee-bg.jpg)`}}>
+                <section className='star section' id='home' style={{backgroundImage: `url(./images/coffee-bg.jpg)`}}>
                     <h1>Relax in a comfortable spot and start your day<br/> with
              your favourite cup of coffee.</h1>
                     <button>Order</button>
--- src/components/UniqueCatagory/UniqueCatagory.js
+++ src/components/UniqueCatagory/UniqueCatagory.js
@@ -3,19 +3,51 @@ import './UniqueCatagory.css';
 const UniqueCatagory = () => {
     return (
         <div>
-            <section className='unique' id='unique'>
+            <section className='unique section container' id='unique'>
                 <div className='unique-container'>
                     <div className='unique-box'>
                         <h3>Finest coffees that lift your spirits and make you happy!</h3>
                     </div>
 
 <div className='unique-catagory'>
-    <div className='special-group'>
-<div className='unique-img-border'>
-    <img src="./images/" alt="" />
-</div>
+
+<div className='unique-coffees'>
+    <div className='coffee'>
+    <img src="./images/Latte coffee.jpg" alt="" />
     </div>
-</div>
+    <h2>Latte Coffee</h2>
+    </div>
+
+    <div className='unique-coffees'>
+    <div className='coffee'>
+    <img src="./images/mocha-coffee.jpg" alt="" />
+    </div>
+    <h2>Mocha Coffee</h2>
+    </div>
+
+    <div className='unique-coffees'>
+    <div className='coffee'>
+    <img src="./images/americano-iced-coffee.jpg" alt="" />
+    </div>
+    <h2>Americano Iced Coffee</h2>
+    </div>
+
+    <div className='unique-coffees'>
+    <div className='coffee'>
+    <img src="./images/cappuccino-coffee.jpg" alt="" />
+    </div>
+    <h2>Cappuccino Coffee</h2>
+    </div>
+
+    <div className='unique-coffees'>
+    <div className='coffee'>
+    <img src="./images/americano-hot-coffee.jpg" alt="" />
+    </div>
+    <h2>Americano Hot Coffee</h2>
+    </div>
+   
+
+                </div>
                 </div>
             </section>
         </div>
