98e98db8178e9aafcadf89d81a498e1f5a1a7530
Rain44556 <bristy44556@gmail.com>
2023-11-13 21:39:13 +0600

    window.scrollTo add for order-button in star.js file

--- src/components/StarSection/StarSection.js
+++ src/components/StarSection/StarSection.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import './StarSection.css';
 const StarSection = () => {
+
+    const Order = () =>{
+    window.scrollTo({
+    top: 1150,
+    behavior: 'smooth'
+        })
+            }
+
     return (
         <div>
             <main className='main' id='main'>
                 <section className='star section' id='home' style={{backgroundImage: `url(./images/coffee-bg.jpg)`}}>
                     <h1>Relax in a comfortable spot and start your day<br/> with
              your favourite cup of coffee.</h1>
-                    <button>Order</button>
+                    <button onClick={Order}>Order</button>
                 </section>
             </main>
         </div>
