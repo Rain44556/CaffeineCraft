5441aefb4ffb98668e61bebc33466164350a769e
Rain44556 <bristy44556@gmail.com>
2023-11-13 22:29:38 +0600

    items files added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import Header from "./components/Header/Header.js";
 import StarSection from './components/StarSection/StarSection.js';
 import UniqueCatagory from './components/UniqueCatagory/UniqueCatagory.js';
+import Items from './components/Items/Items';
+
 
 function App() {
   return (
@@ -9,6 +11,7 @@ function App() {
   <Header></Header>
   <StarSection></StarSection>
   <UniqueCatagory></UniqueCatagory>
+  <Items></Items>
 </div>
   );
 }
---
+++ src/components/Items/Items.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Items = () => {
+    return (
+        <div>
+            <section>
+                <div>
+                    <h3>Locate appetising cuisine and<br/>
+                    select your favourites.</h3>
+                </div>
+            </section>
+        </div>
+    );
+};
+
+export default Items;
