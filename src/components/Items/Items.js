a6a35d33885573fa982b77120173ee1454c040fc
Rain44556 <bristy44556@gmail.com>
2023-11-14 22:24:41 +0600

    json file add

--- src/components/Items/Items.js
+++ src/components/Items/Items.js
@@ -1,12 +1,20 @@
 import React from 'react';
-
+import './Items.css';
 const Items = () => {
     return (
         <div>
             <section>
                 <div>
-                    <h3>Locate appetising cuisine and<br/>
+                <h3>Locate appetising cuisine and<br/>
                     select your favourites.</h3>
+                <ul className="items-filter">
+                    <li className='items-product items-line active-item' data-filter='.'>
+                <h4>Main Menu</h4>
+                <span>3products</span>
+                    </li>
+                </ul>
+                
+                
                 </div>
             </section>
         </div>
