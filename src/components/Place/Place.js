36af037f9a390f15a6bda69c04c3155e76af81cf
Rain44556 <bristy44556@gmail.com>
2023-11-23 12:45:07 +0600

    place page are ready to go!

--- src/components/Place/Place.js
+++ src/components/Place/Place.js
@@ -9,29 +9,34 @@ const Place = () => {
 
     return (
         <div>
-            <section className='place' id='place'>
-<div className='place-container'>
-    <h3>bring you comfort With our cosy services!</h3>
-    <div className='place-content'>
-    <div className='img-place'>
-        <img src={imageSelect} className='img-place-big'/>
+         <section className='place' id='place'>
+         <div className='place-container'>
+         <h3>bring you comfort With our cosy services!</h3>
+           
+         <div className='place-content'>
 
-    <div className='img-place-smalls'>
-    {data.places.map((place,index) =>{
-    return <img key={index}
+         <div className='img-place'>
+         <img src={imageSelect} className='img-place-big'/>
+
+         <div className='img-place-smalls'>
+            {data.places.map((place,index) =>{
+        return <img key={index}
         src={place.imageUrl}
         alt=''
         onClick={() => setImageSelect(place.imageUrl)}
         className= {`img-place-small ${imageSelect === place.imageUrl && 'active'}`}/>
-    })}
+        })}
 
-    <div>
-        <h2></h2>
-        <p></p>
-    </div>
-
-    </div>
         </div>
+        </div>
+
+
+    <div className='place-details'>
+        <h2 className='title'>Locate a location that you admire</h2>
+        <p className='description'>We offer the ideal environment, complete with lovely decor and 
+        <br /> positive energy, for continued productivity and focus</p>
+    </div>
+
     </div>
         </div>
             </section>
