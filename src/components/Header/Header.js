e49e32d4b8dce8e941f9de00a30e1de7b853509a
rain44556 <bristy44556@gmail.com>
2025-05-15 23:49:03 +0600

    Implemented hamburger toggle functionality and Styled mobile menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,43 +1,70 @@
-import React, { useEffect, useState } from 'react';
-import './Header.css';
+import React, { useEffect, useState } from "react";
+import "./Header.css";
 
 const Header = () => {
-    const [changeHeader,setChangeHeader] = useState(false);
-    
-   useEffect(() => {
-     const changeHeaderColor = () =>{
-        if (window.scrollY >= 50){
-            setChangeHeader(true)
-        } else{
-            setChangeHeader(false)
-        }
+  const [changeHeader, setChangeHeader] = useState(false);
+const [showMenu, setShowMenu] = useState(false);
+
+ 
+    const changeHeaderColor = () => {
+      if (window.scrollY >= 50) {
+        setChangeHeader(true);
+      } else {
+        setChangeHeader(false);
+      }
     };
-    window.addEventListener('scroll', changeHeaderColor);
-    
-    // Cleanup on unmount
-    return ()=>{
-            window.addEventListener('scroll', changeHeaderColor)
-    }
-   }, []);
+
+    window.addEventListener("scroll", changeHeaderColor);
 
 
-    return (
-        <header className= {`header ${changeHeader ? "scroll-header" : ""}`} id='header'>
-            <nav className='nav-bar container'>
-                <a href="#home" className='logo'><span>Caffeine</span>Craft</a>
+  return (
+    <header
+      className={`header ${changeHeader ? "scroll-header" : ""}`}
+      id="header"
+    >
+      <nav className="nav-bar container">
+        <a href="#home" className="logo">
+          <span>Caffeine</span>Craft
+        </a>
 
-<div className="menu" id='menu'>
-<ul className='lists'>
-    <li><a href="#place" className='navbar-link'>Place</a></li>
-    <li><a href="#items" className='navbar-link'>Items</a></li>
-    <li><a href="#home" className='navbar-link active-link'>Home</a></li>
-</ul>
-</div>
-</nav>
-</header>
+        {/* Hamburger menu */}
+        <button 
+        onClick={() => setShowMenu(!showMenu)}
+        className="hamburger-menu"
+        aria-label="Toggle-menu"
+        >
+            <i className="bx bx-menu"></i>
+        </button>
 
-
-    );
+            {/* menu */}
+        <div className={`menu ${showMenu ? 'show-menu' : ""}`} id="menu">
+          <ul className="lists">
+            <li>
+              <a href="#place" 
+              className="navbar-link"
+              onClick={()=> setShowMenu(false)}>
+                Place
+              </a>
+            </li>
+            <li>
+              <a href="#items" 
+              className="navbar-link"
+              onClick={()=> setShowMenu(false)}>
+                Items
+              </a>
+            </li>
+            <li>
+              <a href="#home" 
+              className="navbar-link active-link"
+              onClick={()=> setShowMenu(false)}>
+                Home
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
